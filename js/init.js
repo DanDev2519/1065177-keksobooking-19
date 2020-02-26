e8561a54d5e76cddc22d13b327611c9fd42eac12
DanDev2519 <ostryakov.danil@yandex.ru>
2020-02-26 22:36:14 +0300

    Исправление функци в файле init.js

--- js/init.js
+++ js/init.js
@@ -1,13 +1,17 @@
 'use strict';
 
-window.addEventListener('load', function () {
+(function () {
+
   var adForm = document.querySelector('.ad-form');
   var mapFilters = document.querySelector('.map__filters');
 
   // Функция инициализации
-  (function () {
+  window.addEventListener('load', function () {
+  // (function () {
     window.utils.setAttributeDisabledChildren(adForm);
     window.utils.setAttributeDisabledChildren(mapFilters);
     window.form.setInitUserAdressInput(false);
-  })();
-});
+  // })();
+  });
+
+})();
