051b724b07e40191fe50047a994962729db6c124
DanDev2519 <ostryakov.danil@yandex.ru>
2020-03-10 22:54:49 +0300

    Добавление фильтров по количеству комнат, гостей и цене

--- js/filters.js
+++ js/filters.js
@@ -6,17 +6,37 @@
 
   var mapFilters = document.querySelector('.map__filters');
   var housingTypeFilter = mapFilters.querySelector('#housing-type');
+  var housingPriceFilter = mapFilters.querySelector('#housing-price');
+  var housingRoomsFilter = mapFilters.querySelector('#housing-rooms');
+  var housingGuestsFilter = mapFilters.querySelector('#housing-guests');
 
 
   var filterByHousingType = function (it) {
     return it.offer.type === housingTypeFilter.value || housingTypeFilter.value === 'any';
+  };
+  var filterByHousingPrice = function (it) {
+    var priceRang = 'any';
+    if (it.offer.price < 10000) {
+      priceRang = 'low';
+    } else if (it.offer.price > 50000) {
+      priceRang = 'high';
+    } else if (it.offer.price >= 10000 || it.offer.price <= 50000) {
+      priceRang = 'middle';
+    }
+    return priceRang === housingPriceFilter.value || housingPriceFilter.value === 'any';
+  };
+  var filterByHousingRooms = function (it) {
+    return it.offer.rooms === +housingRoomsFilter.value || housingRoomsFilter.value === 'any';
+  };
+  var filterByHousingGuests = function (it) {
+    return it.offer.guests === +housingGuestsFilter.value || housingGuestsFilter.value === 'any';
   };
 
   var filterPins = function () {
     var i = 0;
     var resultArray = [];
     while (i < pins.length && resultArray.length < NUMBER_OF_ADS) {
-      if (filterByHousingType(pins[i])) {
+      if (filterByHousingType(pins[i]) && filterByHousingPrice(pins[i]) && filterByHousingRooms(pins[i]) && filterByHousingGuests(pins[i])) {
         resultArray.push(pins[i]);
       }
       i++;
