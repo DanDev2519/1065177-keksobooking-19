80eb01ca2c27c2301968cd98bc1fb1436725a78b
DanDev2519 <ostryakov.danil@yandex.ru>
2020-03-15 14:45:48 +0300

    Удаление data.js, чистка файла utils/js от неиспольземого кода

--- js/utils.js
+++ js/utils.js
@@ -18,36 +18,6 @@
       action();
     }
   };
-
-  // Функция создания строки из числа с ведущими нулями
-  var getLeadingZeros = function (num, size) {
-    var s = num.toString();
-    while (s.length < size) {
-      s = '0' + s;
-    }
-    return s;
-  };
-  // Функция получения случайного целого числа из диапазона, не сключая max
-  var getRandomInt = function (min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  };
-  // Функция получения случайного целого числа из диапазона, включительно
-  var getRandomIntInclusive = function (min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-  // Функция генерации массива случайной длины из другого массива с вариантам без повтора
-  var getArrayNoRepeatFrom = function (arrayIn) {
-    var arrayOut = [];
-    var length = getRandomIntInclusive(0, arrayIn.length);
-    for (var i = 0; i < length; i++) {
-      var arrayEl = arrayIn[getRandomInt(0, arrayIn.length)];
-      while (arrayOut.indexOf(arrayEl) !== -1) {
-        arrayEl = arrayIn[getRandomInt(0, arrayIn.length)];
-      }
-      arrayOut.push(arrayEl);
-    }
-    return arrayOut;
-  };
   // Функция добавления атрибута disabled дочерним элементам
   var setAttributeDisabledChildren = function (parent) {
     Array.from(parent.children).forEach(function (child) {
@@ -79,10 +49,6 @@
     // window.utils.
     isEscEvent: isEscEvent,
     isEnterEvent: isEnterEvent,
-    getLeadingZeros: getLeadingZeros,
-    getRandomInt: getRandomInt,
-    getRandomIntInclusive: getRandomIntInclusive,
-    getArrayNoRepeatFrom: getArrayNoRepeatFrom,
     setAttributeDisabledChildren: setAttributeDisabledChildren,
     removeAttributeDisabledChildren: removeAttributeDisabledChildren,
     debounce: debounce
